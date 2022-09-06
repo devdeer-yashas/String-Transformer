8babf23babadeba412f5a24607c9d81b7788612d
devdeer <yashas.manjunath@devdeer.com>
2022-09-06 13:24:30 +0200

    Implemented BaseTypes

---
+++ Logic.Core/BaseTypes.cs
@@ -0,0 +1,20 @@
+namespace devdeer.StringManipulator.Logic.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Interfaces;
+
+    public class BaseTypes : ITransformer
+    {
+        #region explicit interfaces
+
+        public void Transform(string message, List<int> transformationId)
+        {
+            
+        }
+
+        #endregion
+    }
+}
