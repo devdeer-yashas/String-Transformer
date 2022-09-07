5929deeb90ca4f0e50e7311c10ab4afc4fe8bf05
devdeer <yashas.manjunath@devdeer.com>
2022-09-07 15:52:18 +0200

    Created a Ui code for read and write messages for the console.

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-
 using System;
+using System.Linq;
 
-Console.WriteLine("Hello, World!");
+namespace devdeer.StringManipulator.Ui.Console
+{
+    using Console = System.Console;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DisplayMenu();
+            UserInput();
+
+        }
+
+        private static void UserInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("Enter the String to be transformed");
+            Console.WriteLine("Enter the transformations to be applied");
+            Console.WriteLine("1.LowerCaseTransformer");
+            Console.WriteLine("2.UpperCaseTransformer");
+            Console.WriteLine("3.RevertTransformer");
+            Console.WriteLine("4.RemoveItemTransformer");
+            Console.WriteLine("5.ReplaceItemTransformer");
+            
+            
+        }
+    }
+}
